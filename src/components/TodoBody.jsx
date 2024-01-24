1e845aff2d0321ce4f85628aac0c84772a456849
liMoHa <dudgk6382@gmail.com>
2024-01-24 13:27:39 +0900

    add: TodoBody style

--- src/components/TodoBody.jsx
+++ src/components/TodoBody.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import classes from "./TodoBody.module.css";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegSquare } from "react-icons/fa";
 
 export default function TodoBody() {
   return (
-    <div>TodoBody</div>
-  )
+    <div className={classes.body}>
+      <ul className={classes.list}>
+        <li className={classes.item}>
+          <div className={classes.todo}>
+            <button  className={classes.checkbox}>
+              <FaRegSquare />
+            </button>
+            <span>공부</span>
+          </div>
+          <button className={classes.trash}>
+            <FaRegTrashAlt />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
