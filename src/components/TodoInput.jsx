add227135a1c6101f479ad2f80e5b3757bcecb5d
liMoHa <dudgk6382@gmail.com>
2024-01-24 13:45:49 +0900

    add: TodoInput styling

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import classes from "./TodoInput.module.css";
 
 export default function TodoInput() {
   return (
-    <div>TodoInput</div>
-  )
+    <form className={classes.form}>
+      <input className={classes.input} placeholder="Add Todo" type="text" />
+      <button className={classes.button}>Add</button>
+    </form>
+  );
 }
